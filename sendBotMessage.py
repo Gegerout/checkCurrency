df032844d2ef5d9cc4246dbab78df64d27e5097c
Selim Poladov <72309553+Gegerout@users.noreply.github.com>
2023-01-04 16:02:04 +0300

    Update sendBotMessage.py

--- sendBotMessage.py
+++ sendBotMessage.py
@@ -1,6 +1,8 @@
 import requests
-TOKEN = ""
-chat_id = ""
+import os
+
+TOKEN = os.environ['TOKEN']
+chat_id = os.environ['chat_id']
 
 def sendCurrencyValue(value):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text=Current price is {value}"
