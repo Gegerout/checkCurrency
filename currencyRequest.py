a2d233a815ea36011b46986f397833ddedc5463a
Selim Poladov <72309553+Gegerout@users.noreply.github.com>
2023-01-06 14:28:07 +0300

    Update currencyRequest.py

--- currencyRequest.py
+++ currencyRequest.py
@@ -2,6 +2,7 @@ import subprocess
 import sys
 
 subprocess.check_call([sys.executable, "-m", "pip", "install", 'selenium'])
+subprocess.check_call([sys.executable, "-m", "pip", "install", 'sentence-transformers'])
 
 import time
 from selenium import webdriver
