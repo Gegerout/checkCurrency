b573a36a246f2a09ac8b59942774e50abd53688d
Selim Poladov <72309553+Gegerout@users.noreply.github.com>
2023-01-07 13:46:34 +0300

    Create hello.py

---
+++ app/hello.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+import currencyRequest
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def index():
+    result = currencyRequest.sendRequest()
+    return render_template("index.html", result=result)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
