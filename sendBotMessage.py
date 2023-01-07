88b59bf6bef92002a61ae4025841a55f85ba849d
Selim Poladov <72309553+Gegerout@users.noreply.github.com>
2023-01-07 13:49:56 +0300

    Delete sendBotMessage.py

--- sendBotMessage.py
+++
@@ -1,9 +0,0 @@
-import requests
-import os
-
-TOKEN = os.environ['TOKEN']
-chat_id = os.environ['chat_id']
-
-def sendCurrencyValue(value):
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text=Current price is {value}"
-    requests.get(url).json()
