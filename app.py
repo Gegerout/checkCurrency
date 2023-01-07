c68f034c78480acf8ffff10af1ac73bae57ac647
Selim Poladov <72309553+Gegerout@users.noreply.github.com>
2023-01-07 13:04:15 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import currencyRequest
+
+class Server(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+        # get the page
+        path = self.path
+        if path == '/':
+            currencyRequest.sendRequest()
+            with open('templates/index.html', 'rb') as page:
+                self.wfile.write(page.read())
+
+
+try:
+    server = HTTPServer(('localhost', 8080), Server)
+    server.serve_forever()
+except:
+    pass
+
+server.server_close()
