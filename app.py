873ede21d0a93bdfefa97e753ccc0bbe478d9aa4
Selim Poladov <72309553+Gegerout@users.noreply.github.com>
2023-01-07 13:37:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ def index():
 
 if __name__=="__main__":
     app.run(host=os.getenv('IP', '0.0.0.0'),
-            port=int(os.getenv('PORT', 4444)))
+            port=int(os.getenv('PORT', 8080)))
